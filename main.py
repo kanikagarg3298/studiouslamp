6a2795dcca6c7a939a61f84609cdbf15a7461bc0
kanika garg <kanikagarg725@gmail.com>
2018-04-04 00:07:32 +0530

    started the project

---
+++ main.py
@@ -0,0 +1,7 @@
+print 'Hello!'
+name=raw_input('Enter your name')
+print ' Welcome ' +name+ ' '+' glad to back you have with us.'
+salutation =raw_input('What should we call you(Mr. or Ms.) ')
+print salutation+" "+name
+print 'Alright ' +salutation+' '+name+' . I\'d like to know a little bit more about...'
+
